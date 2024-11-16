17c7424c3e76a683428aaeb1290d09a94681e510
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-11-16 23:11:53 +0700

    system

---
+++ system/helpers/array_helper.php
@@ -0,0 +1,186 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2019 - 2022, CodeIgniter Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @copyright	Copyright (c) 2019 - 2022, CodeIgniter Foundation (https://codeigniter.com/)
+ * @license	https://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * CodeIgniter Array Helpers
+ *
+ * @package		CodeIgniter
+ * @subpackage	Helpers
+ * @category	Helpers
+ * @author		EllisLab Dev Team
+ * @link		https://codeigniter.com/userguide3/helpers/array_helper.html
+ */
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('element'))
+{
+	/**
+	 * Element
+	 *
+	 * Lets you determine whether an array index is set and whether it has a value.
+	 * If the element is empty it returns NULL (or whatever you specify as the default value.)
+	 *
+	 * @param	string
+	 * @param	array
+	 * @param	mixed
+	 * @return	mixed	depends on what the array contains
+	 */
+	function element($item, array $array, $default = NULL)
+	{
+		return array_key_exists($item, $array) ? $array[$item] : $default;
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('random_element'))
+{
+	/**
+	 * Random Element - Takes an array as input and returns a random element
+	 *
+	 * @param	array
+	 * @return	mixed	depends on what the array contains
+	 */
+	function random_element($array)
+	{
+		return is_array($array) ? $array[array_rand($array)] : $array;
+	}
+}
+
+// --------------------------------------------------------------------
+
+if ( ! function_exists('elements'))
+{
+	/**
+	 * Elements
+	 *
+	 * Returns only the array items specified. Will return a default value if
+	 * it is not set.
+	 *
+	 * @param	array
+	 * @param	array
+	 * @param	mixed
+	 * @return	mixed	depends on what the array contains
+	 */
+	function elements($items, array $array, $default = NULL)
+	{
+		$return = array();
+
+		is_array($items) OR $items = array($items);
+
+		foreach ($items as $item)
+		{
+			$return[$item] = array_key_exists($item, $array) ? $array[$item] : $default;
+		}
+
+		return $return;
+	}
+}
+
+// --------------------------------------------------------------------
+
+if (!function_exists('array_to_object')) {
+	/**
+	 * Function array_to_object
+	 *
+	 * @param array|mixed $array
+	 *
+	 * @return array|false|\stdClass
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 07/27/2021 36:10
+	 */
+	function array_to_object($array = array())
+	{
+		if (!is_array($array)) {
+			return $array;
+		}
+		$object = new stdClass();
+		$countArray = count($array);
+		if ($countArray > 0) {
+			foreach ($array as $name => $value) {
+				if (!empty($name)) {
+					$object->$name = array_to_object($value);
+				}
+			}
+
+			return $object;
+		}
+
+		return false;
+	}
+}
+
+// --------------------------------------------------------------------
+
+if ( ! function_exists('array_to_attr'))
+{
+	/**
+	 * Takes an array of attributes and turns it into a string for an html tag
+	 *
+	 * @param	array	$attr
+	 * @return	string
+	 */
+	function array_to_attr($attr)
+	{
+		$attr_str = '';
+
+		foreach ((array) $attr as $property => $value)
+		{
+			// Ignore null/false
+			if ($value === null or $value === false)
+			{
+				continue;
+			}
+
+			// If the key is numeric then it must be something like selected="selected"
+			if (is_numeric($property))
+			{
+				$property = $value;
+			}
+
+			$attr_str .= $property.'="'.str_replace('"', '&quot;', $value).'" ';
+		}
+
+		// We strip off the last space for return
+		return trim($attr_str);
+	}
+}
---
+++ system/helpers/string_helper.php
@@ -0,0 +1,386 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2019 - 2022, CodeIgniter Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @copyright	Copyright (c) 2019 - 2022, CodeIgniter Foundation (https://codeigniter.com/)
+ * @license	https://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * CodeIgniter String Helpers
+ *
+ * @package		CodeIgniter
+ * @subpackage	Helpers
+ * @category	Helpers
+ * @author		EllisLab Dev Team
+ * @link		https://codeigniter.com/userguide3/helpers/string_helper.html
+ */
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('trim_slashes'))
+{
+	/**
+	 * Trim Slashes
+	 *
+	 * Removes any leading/trailing slashes from a string:
+	 *
+	 * /this/that/theother/
+	 *
+	 * becomes:
+	 *
+	 * this/that/theother
+	 *
+	 * @todo	Remove in version 3.1+.
+	 * @deprecated	3.0.0	This is just an alias for PHP's native trim()
+	 *
+	 * @param	string
+	 * @return	string
+	 */
+	function trim_slashes($str)
+	{
+		return trim($str, '/');
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('strip_slashes'))
+{
+	/**
+	 * Strip Slashes
+	 *
+	 * Removes slashes contained in a string or in an array
+	 *
+	 * @param	mixed	string or array
+	 * @return	mixed	string or array
+	 */
+	function strip_slashes($str)
+	{
+		if ( ! is_array($str))
+		{
+			return stripslashes($str);
+		}
+
+		foreach ($str as $key => $val)
+		{
+			$str[$key] = strip_slashes($val);
+		}
+
+		return $str;
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('strip_quotes'))
+{
+	/**
+	 * Strip Quotes
+	 *
+	 * Removes single and double quotes from a string
+	 *
+	 * @param	string
+	 * @return	string
+	 */
+	function strip_quotes($str)
+	{
+		return str_replace(array('"', "'"), '', $str);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('quotes_to_entities'))
+{
+	/**
+	 * Quotes to Entities
+	 *
+	 * Converts single and double quotes to entities
+	 *
+	 * @param	string
+	 * @return	string
+	 */
+	function quotes_to_entities($str)
+	{
+		return str_replace(array("\'","\"","'",'"'), array("&#39;","&quot;","&#39;","&quot;"), $str);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('reduce_double_slashes'))
+{
+	/**
+	 * Reduce Double Slashes
+	 *
+	 * Converts double slashes in a string to a single slash,
+	 * except those found in http://
+	 *
+	 * http://www.some-site.com//index.php
+	 *
+	 * becomes:
+	 *
+	 * http://www.some-site.com/index.php
+	 *
+	 * @param	string
+	 * @return	string
+	 */
+	function reduce_double_slashes($str)
+	{
+		return preg_replace('#(^|[^:])//+#', '\\1/', $str);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('reduce_multiples'))
+{
+	/**
+	 * Reduce Multiples
+	 *
+	 * Reduces multiple instances of a particular character.  Example:
+	 *
+	 * Fred, Bill,, Joe, Jimmy
+	 *
+	 * becomes:
+	 *
+	 * Fred, Bill, Joe, Jimmy
+	 *
+	 * @param	string
+	 * @param	string	the character you wish to reduce
+	 * @param	bool	TRUE/FALSE - whether to trim the character from the beginning/end
+	 * @return	string
+	 */
+	function reduce_multiples($str, $character = ',', $trim = FALSE)
+	{
+		$str = preg_replace('#'.preg_quote($character, '#').'{2,}#', $character, $str);
+		return ($trim === TRUE) ? trim($str, $character) : $str;
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if (!function_exists('random_string')) {
+	/**
+	 * Creates a random string of characters
+	 *
+	 * From FuelPHP
+	 *
+	 * @param string $type   the type of string
+	 * @param int    $length the number of characters
+	 *
+	 * @return  string  the random string
+	 */
+	function random_string($type = 'alnum', $length = 16)
+	{
+		switch ($type) {
+			case 'basic':
+				return mt_random_int();
+				break;
+
+			default:
+			case 'alnum':
+			case 'numeric':
+			case 'nozero':
+			case 'alpha':
+			case 'distinct':
+			case 'hexdec':
+				switch ($type) {
+					case 'alpha':
+						$pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+						break;
+
+					default:
+					case 'alnum':
+						$pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+						break;
+
+					case 'numeric':
+						$pool = '0123456789';
+						break;
+
+					case 'nozero':
+						$pool = '123456789';
+						break;
+
+					case 'distinct':
+						$pool = '2345679ACDEFHJKLMNPRSTUVWXYZ';
+						break;
+
+					case 'hexdec':
+						$pool = '0123456789abcdef';
+						break;
+				}
+
+				$str = '';
+				for ($i = 0; $i < $length; $i++) {
+					$str .= substr($pool, mt_random_int(0, strlen($pool) - 1), 1);
+				}
+
+				return $str;
+				break;
+
+			case 'md5':
+			case 'unique':
+				return md5(uniqid(mt_random_int(), true));
+				break;
+			case 'base64':
+				return base64_encode(md5(uniqid(mt_random_int(), true)));
+				break;
+			case 'sha1' :
+				return sha1(uniqid(mt_random_int(), true));
+				break;
+			case 'sha256' :
+				return hash('sha256', uniqid(mt_random_int(), true));
+				break;
+			case 'sha384' :
+				return hash('sha384', uniqid(mt_random_int(), true));
+				break;
+			case 'sha512' :
+				return hash('sha512', uniqid(mt_random_int(), true));
+				break;
+			case 'whirlpool' :
+				return hash('whirlpool', uniqid(mt_random_int(), true));
+				break;
+			case 'uuid':
+				$pool = array('8', '9', 'a', 'b');
+
+				return sprintf('%s-%s-4%s-%s%s-%s',
+							   random_string('hexdec', 8),
+							   random_string('hexdec', 4),
+							   random_string('hexdec', 3),
+							   $pool[array_rand($pool)],
+							   random_string('hexdec', 3),
+							   random_string('hexdec', 12)
+				);
+				break;
+			case 'binary':
+				if (function_exists('random_bytes')) {
+					try {
+						return random_bytes($length);
+					}catch (\Exception $e){
+						log_message('debug', 'Error Code: '.$e->getCode().' - File: '.$e->getFile().' - Line: '.$e->getLine().' - Message: '.$e->getMessage());
+						return null;
+					}
+				} else {
+					return null;
+				}
+				break;
+			case 'hex':
+			case 'crypto':
+				if ($length % 2 !== 0) {
+					throw new InvalidArgumentException('You must set an even number to the second parameter when you use `crypto`.');
+				}
+				if (function_exists('random_bytes')) {
+					try {
+						return bin2hex(random_bytes($length / 2));
+					}catch (\Exception $e){
+						log_message('debug', 'Error Code: '.$e->getCode().' - File: '.$e->getFile().' - Line: '.$e->getLine().' - Message: '.$e->getMessage());
+						return null;
+					}
+				} else {
+					return null;
+				}
+				break;
+		}
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('increment_string'))
+{
+	/**
+	 * Add's _1 to a string or increment the ending number to allow _2, _3, etc
+	 *
+	 * @param	string	required
+	 * @param	string	What should the duplicate number be appended with
+	 * @param	string	Which number should be used for the first dupe increment
+	 * @return	string
+	 */
+	function increment_string($str, $separator = '_', $first = 1)
+	{
+		preg_match('/(.+)'.preg_quote($separator, '/').'([0-9]+)$/', $str, $match);
+		return isset($match[2]) ? $match[1].$separator.($match[2] + 1) : $str.$separator.$first;
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('alternator'))
+{
+	/**
+	 * Alternator
+	 *
+	 * Allows strings to be alternated. See docs...
+	 *
+	 * @param	string (as many parameters as needed)
+	 * @return	string
+	 */
+	function alternator()
+	{
+		static $i;
+
+		if (func_num_args() === 0)
+		{
+			$i = 0;
+			return '';
+		}
+
+		$args = func_get_args();
+		return $args[($i++ % count($args))];
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('repeater'))
+{
+	/**
+	 * Repeater function
+	 *
+	 * @todo	Remove in version 3.1+.
+	 * @deprecated	3.0.0	This is just an alias for PHP's native str_repeat()
+	 *
+	 * @param	string	$data	String to repeat
+	 * @param	int	$num	Number of repeats
+	 * @return	string
+	 */
+	function repeater($data, $num = 1)
+	{
+		return ($num > 0) ? str_repeat($data, $num) : '';
+	}
+}
