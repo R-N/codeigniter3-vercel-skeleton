149f9fd56b214caff92eab3b3251050d9581735e
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-11-18 11:17:58 +0700

    Update migrate.lisp

--- migrate.lisp
+++ migrate.lisp
@@ -1,6 +1,6 @@
 /* content of the script.lisp */
 LOAD DATABASE 
-FROM mysql://root:@localhost:3306/albumtravel 
-INTO postgres://Linearch:@localhost:5432/postgres?sslmode=allow 
+FROM mysql://root:@localhost:3306/database 
+INTO postgres://postgres:@localhost:5432/postgres?sslmode=allow 
 WITH include drop, create tables, create indexes, reset sequences 
 ;
